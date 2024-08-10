d0a33a709608ca0da75ee21b62b8e4a3fc61c09c
Yury <{ID}+{username}@users.noreply.github.com>
2024-08-10 11:47:07 +0200

    add anonymizer

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import { Telegraf } from 'telegraf'
-import { message } from 'telegraf/filters'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('This bot can send your msg to admins'))
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply(`Welcome 👋\\! \nI can help you to send the message to admins\\. 
+In the middle way feel free to use \`/unwrap\` command which helps you to anonymize your tiktok links`, {
+    parse_mode: 'MarkdownV2'
+}));
+bot.help((ctx) => ctx.reply('This bot can send your msg to admins'));
 
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
 bot.command('quit', async (ctx) => {
     // Explicit usage
     await ctx.telegram.leaveChat(ctx.message.chat.id).then(async () => {
@@ -17,26 +21,22 @@ bot.command('quit', async (ctx) => {
         await ctx.telegram.sendMessage(ctx.message.chat.id, `🔑 Sorry cannot leave private chat`);
         console.log(err);
     })
-
-    // Using context shortcut
-    await ctx.leaveChat();
-})
+});
 
 bot.command('msg', async (ctx) => {
-
     await ctx.telegram.sendMessage(ctx.message.chat.id, `Sending your msg to admins...`);
-
-    //await ctx.telegram.sendMessage(ctx.message.chat.id, `${ctx.message.text}`);
+    //const messageText = ctx.message.text.replace('/unwrap', '').trim();
     await ctx.telegram.sendMessage(ctx.message.chat.id, 'Sorry the bot does not work now');
-})
-// bot.command('help', async (ctx) => {
-//     // Explicit usage
-//     await ctx.telegram.sendMessage(ctx.message.chat.id, `This bot can send your msg to admins`);
+});
 
-// })
+bot.command('unwrap', async (ctx) => {
+    const resp = await fetch(ctx.payload);
+    const anonymUrl = resp.url.split('?')[0]
+    await ctx.telegram.sendMessage(ctx.message.chat.id, `Unwrapped url is \`${anonymUrl}\``);
+});
 
 bot.launch()
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
